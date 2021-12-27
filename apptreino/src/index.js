ebaffd6c5244ff2fd1e2118d1abc32e13d7b9553
Héricles <haclesrocha200@gmail.com>
2021-12-27 15:09:21 -0100

    Primeira tela criada

--- apptreino/src/index.js
+++ apptreino/src/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+// View: div, footer, header, main, aside, section
+// Text: p, span, strong, h1, h2, h3
+
+// Todos os componentes possuem por padrao display flex
+// elementos nao possuem significado
 
 export default function App() {
-
-    return <View />;
+    return (
+    <>   
+      <StatusBar barStyle="light-content" backgroundColor="#140CF1"/>
+      <View style={styles.container}>
+        <Text style={styles.title}>Treinamento</Text>
+      </View>
+    </>
+    );
 }
+ 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#140CF1',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    title: {
+        color: #FFF,
+        fontSize: 32,
+        fontWeight: 'bold'
+    },
+});
